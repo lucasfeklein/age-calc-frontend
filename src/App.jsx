a0f7d68f3edcf9953c11dc5c9d4b6385d4a288b9
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-19 15:44:11 +0800

    invalid alert

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,15 @@ function App() {
   });
 
   const [validation, setValidation] = useState({
+    valid: true,
     day: true,
     month: true,
     year: true,
   });
 
   const { day, month, year } = dateObject;
+
+  const validColor = validation.valid ? "" : "hsl(0, 100%, 67%)";
 
   const handleInput = (e) => {
     const { name, value } = e.target;
@@ -61,43 +64,63 @@ function App() {
   };
 
   const handleClick = () => {
-    // Convert input values to integers
-    const inputDay = parseInt(day, 10);
-    const inputMonth = parseInt(month, 10);
-    const inputYear = parseInt(year, 10);
+    // Validate all inputs before proceeding
+    const isValidDay = validateInput("day", day);
+    const isValidMonth = validateInput("month", month);
+    const isValidYear = validateInput("year", year);
 
-    // Create Date objects for the input date and current date
-    const inputDate = new Date(inputYear, inputMonth - 1, inputDay); // Month is zero-based
-    const currentDate = new Date();
+    if (isValidDay && isValidMonth && isValidYear) {
+      // Convert input values to integers
+      const inputDay = parseInt(day, 10);
+      const inputMonth = parseInt(month, 10);
+      const inputYear = parseInt(year, 10);
 
-    // Calculate the time difference in milliseconds
-    const timeDifference = currentDate - inputDate;
+      // Create Date objects for the input date and current date
+      const inputDate = new Date(inputYear, inputMonth - 1, inputDay); // Month is zero-based
+      const currentDate = new Date();
 
-    // Calculate years, months, and days
-    const years = Math.floor(timeDifference / (365 * 24 * 60 * 60 * 1000));
-    const months = Math.floor(
-      (timeDifference % (365 * 24 * 60 * 60 * 1000)) /
-        (30 * 24 * 60 * 60 * 1000)
-    );
-    const days = Math.floor(
-      ((timeDifference % (365 * 24 * 60 * 60 * 1000)) %
-        (30 * 24 * 60 * 60 * 1000)) /
-        (24 * 60 * 60 * 1000)
-    );
+      // Calculate the time difference in milliseconds
+      const timeDifference = currentDate - inputDate;
 
-    // Update calcObj state with the calculated values
-    setCalcObj({
-      years,
-      months,
-      days,
-    });
+      // Calculate years, months, and days
+      const years = Math.floor(timeDifference / (365 * 24 * 60 * 60 * 1000));
+      const months = Math.floor(
+        (timeDifference % (365 * 24 * 60 * 60 * 1000)) /
+          (30 * 24 * 60 * 60 * 1000)
+      );
+      const days = Math.floor(
+        ((timeDifference % (365 * 24 * 60 * 60 * 1000)) %
+          (30 * 24 * 60 * 60 * 1000)) /
+          (24 * 60 * 60 * 1000)
+      );
+
+      // Update calcObj state with the calculated values
+      setCalcObj({
+        years,
+        months,
+        days,
+      });
+      setValidation({
+        valid: true,
+        day: isValidDay,
+        month: isValidMonth,
+        year: isValidYear,
+      });
+    } else {
+      setValidation({
+        valid: false,
+        day: isValidDay,
+        month: isValidMonth,
+        year: isValidYear,
+      });
+    }
   };
 
   return (
     <div className="container">
       <div className="calc-container">
         <div className="input-container">
-          <label>
+          <label style={{ color: validColor }}>
             DAY
             <input
               placeholder="DD"
@@ -105,9 +128,15 @@ function App() {
               onChange={handleInput}
               name="day"
               type="number"
+              style={{
+                borderColor: validColor,
+              }}
             />
+            {!validation.day && (
+              <p className="invalid-alert">Must be a valid day</p>
+            )}
           </label>
-          <label>
+          <label style={{ color: validColor }}>
             MONTH
             <input
               placeholder="MM"
@@ -115,9 +144,15 @@ function App() {
               onChange={handleInput}
               name="month"
               type="number"
+              style={{
+                borderColor: validColor,
+              }}
             />
+            {!validation.month && (
+              <p className="invalid-alert">Must be a valid month</p>
+            )}
           </label>
-          <label>
+          <label style={{ color: validColor }}>
             YEAR
             <input
               placeholder="YYYY"
@@ -125,7 +160,13 @@ function App() {
               onChange={handleInput}
               name="year"
               type="number"
+              style={{
+                borderColor: validColor,
+              }}
             />
+            {!validation.year && (
+              <p className="invalid-alert">Must be a valid year</p>
+            )}
           </label>
           <button onClick={handleClick}>
             <img src="/icon-arrow.svg" />
