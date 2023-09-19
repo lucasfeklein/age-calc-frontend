38bf04c9c8ff21798192a8ef36da7907cee29c0a
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-19 14:34:12 +0800

    calculate time difference function

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
     year: "",
   });
 
+  const [calcObj, setCalcObj] = useState({
+    years: null,
+    months: null,
+    days: null,
+  });
+
   const { day, month, year } = dateObject;
 
   const handleInput = (e) => {
@@ -16,6 +22,39 @@ function App() {
     }));
   };
 
+  const handleClick = () => {
+    // Convert input values to integers
+    const inputDay = parseInt(day, 10);
+    const inputMonth = parseInt(month, 10);
+    const inputYear = parseInt(year, 10);
+
+    // Create Date objects for the input date and current date
+    const inputDate = new Date(inputYear, inputMonth - 1, inputDay); // Month is zero-based
+    const currentDate = new Date();
+
+    // Calculate the time difference in milliseconds
+    const timeDifference = currentDate - inputDate;
+
+    // Calculate years, months, and days
+    const years = Math.floor(timeDifference / (365 * 24 * 60 * 60 * 1000));
+    const months = Math.floor(
+      (timeDifference % (365 * 24 * 60 * 60 * 1000)) /
+        (30 * 24 * 60 * 60 * 1000)
+    );
+    const days = Math.floor(
+      ((timeDifference % (365 * 24 * 60 * 60 * 1000)) %
+        (30 * 24 * 60 * 60 * 1000)) /
+        (24 * 60 * 60 * 1000)
+    );
+
+    // Update calcObj state with the calculated values
+    setCalcObj({
+      years,
+      months,
+      days,
+    });
+  };
+
   return (
     <div className="container">
       <div className="calc-container">
@@ -23,6 +62,7 @@ function App() {
           <label>
             DAY
             <input
+              placeholder="DD"
               value={day}
               onChange={handleInput}
               name="day"
@@ -32,6 +72,7 @@ function App() {
           <label>
             MONTH
             <input
+              placeholder="MM"
               value={month}
               onChange={handleInput}
               name="month"
@@ -41,25 +82,26 @@ function App() {
           <label>
             YEAR
             <input
+              placeholder="YYYY"
               value={year}
               onChange={handleInput}
               name="year"
               type="number"
             />
           </label>
-          <button>
+          <button onClick={handleClick}>
             <img src="/icon-arrow.svg" />
           </button>
         </div>
         <div className="text-container">
           <h2>
-            <span>-- </span>years
+            <span>{calcObj.years ? calcObj.years : "--"} </span>years
           </h2>
           <h2>
-            <span>-- </span>months
+            <span>{calcObj.months ? calcObj.months : "--"} </span>months
           </h2>
           <h2>
-            <span>-- </span>days
+            <span>{calcObj.days ? calcObj.days : "--"} </span>days
           </h2>
         </div>
       </div>
